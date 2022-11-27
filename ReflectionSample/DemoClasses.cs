547fca1d437b3d582673f1b571b138888183cb14
Zeynel Şahin <50925966+zeynelsahin@users.noreply.github.com>
2022-11-27 19:17:28 +0300

    Reflection metodları metadadataları incelenmesi

---
+++ ReflectionSample/DemoClasses.cs
@@ -0,0 +1,62 @@
+﻿namespace ReflectionSample;
+
+public interface ITalk
+{
+    void Talk(string sentence);
+}
+
+public class EmployeeMarkerAttribute : Attribute
+{
+}
+
+[EmployeeMarker]
+public class Employee : Person
+{
+    public string Company { get; set; }
+}
+
+public class Alien : ITalk
+{
+    public void Talk(string sentence)
+    {
+        Console.WriteLine($"Alien talking : {sentence}");
+    }
+}
+
+public class Person: ITalk
+{
+    public Person() 
+    {
+        Console.WriteLine("A person is being created");
+    }
+
+    public Person(string name)
+    {
+        Console.WriteLine($"A person with name {name} ,s being create");
+        Name = name;
+    }
+
+    public Person(string name, int age)
+    {
+        Console.WriteLine($"A person with name {name} ang age {age} is being created using a private constructor");
+        Name = name;
+        this.age = age;
+    }
+    public string Name { get; set; }
+    public int age;
+    private string _aPrivateField = "initial private field value";
+    public void Talk(string sentence)
+    {
+        Console.WriteLine($"Talking : {sentence}");
+    }
+
+    protected void Yell(string sentence)
+    {
+        Console.WriteLine($"Yelling {sentence}");
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} {age} {_aPrivateField}";
+    }
+}
---
+++ ReflectionSample/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Reflection;
+
+var name = "Zeynel";
+// var nameType = name.GetType();
+var nameType = typeof(string);
+Console.WriteLine(nameType);
+
+var currentAssembly = Assembly.GetExecutingAssembly();
+var typesFromCurrentAssembly = currentAssembly.GetTypes();
+foreach (var type in typesFromCurrentAssembly)
+{
+    Console.WriteLine(type.Name);
+}
+Console.WriteLine("-------------");
+var oneTypeFromCurrentAssembly = currentAssembly.GetType("ReflectionSample.Person");
+Console.WriteLine(oneTypeFromCurrentAssembly.Name);
+
+var externalAssembly = Assembly.Load("System.Text.Json");
+var typesFromExternalAssembly = externalAssembly.GetTypes();
+var oneTypeFromExternalAssembly = externalAssembly.GetType("Sytem.Text.Json.JsonProperty");
+
+var modulesFromExternalAssembly = externalAssembly.GetModules();//Tüm moddüller
+var oneModuleFromExternalAssembly = externalAssembly.GetModule("System.Text.Json.dll");//Tek module dll
+
+var typesFromModuleFromExternalAssembly = oneModuleFromExternalAssembly.GetTypes();
+var oneTypeFromModuleFromExternalAssembly = oneModuleFromExternalAssembly.GetType("System.Text.Json.JsonProperty");
+
+var constructors = oneTypeFromCurrentAssembly.GetConstructors();
+foreach (var constructor in oneTypeFromCurrentAssembly.GetConstructors())//Person clasının  constructorları
+{
+    Console.WriteLine(constructor);
+}
+
+// foreach (var method in oneTypeFromCurrentAssembly.GetMethods())
+// {
+//     Console.WriteLine(method);
+// }
+//
+
+foreach (var method in oneTypeFromCurrentAssembly.GetMethods(BindingFlags.Instance|BindingFlags.Public|BindingFlags.NonPublic))
+{
+    Console.WriteLine($"{method} , public : {method.IsPublic}");
+}
+
+foreach (var method in oneTypeFromCurrentAssembly.GetFields(BindingFlags.Instance|BindingFlags.NonPublic))
+{
+    Console.WriteLine($"{method} , public : {method.IsPublic}");
+}
